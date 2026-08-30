7567579ac651b6c07b1f294887e7dd9ecf1e013f
p8by <250748350+p8by@users.noreply.github.com>
2026-08-31 00:08:01 +0100

    Update ollama
    Now contains a ui

--- modules/ollama.nix
+++ modules/ollama.nix
@@ -14,5 +14,13 @@
         host = "127.0.0.1";
         port = 11434;
       };
+      services.open-webui = {
+        enable = true;
+        package = pkgs.open-webui;
+        environment ={
+          OLLAMA_BASE_URL = "http://127.0.0.1:11434";
+          WEBUI_AUTH = "False";
+        };
+      };
     };
 }
