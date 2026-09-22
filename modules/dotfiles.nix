97c070d3fa7780a0041f9857cedf66b5b76f2330
p8by <250748350+p8by@users.noreply.github.com>
2026-09-22 15:36:44 +0100

    More dots

--- modules/dotfiles.nix
+++ modules/dotfiles.nix
@@ -6,5 +6,13 @@
       config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-dotfiles/dotfiles/fastfetch/config.jsonc";
     xdg.configFile."driftwm/config.toml".source =
       config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-dotfiles/dotfiles/driftwm/config.toml";
+    xdg.configFile."wofi/config".source =
+      config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-dotfiles/dotfiles/wofi/config";
+    xdg.configFile."wofi/style.css".source =
+      config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-dotfiles/dotfiles/wofi/style.css";
+    xdg.configFile."waybar/config.jsonc".source =
+      config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-dotfiles/dotfiles/waybar/config.jsonc";
+    xdg.configFile."waybar/style.css".source =
+      config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-dotfiles/dotfiles/waybar/style.css";
   };
 }
--- modules/packages.nix
+++ modules/packages.nix
@@ -35,6 +35,7 @@
     waybar
     scrot
     feh
+    playerctl
     ###
 
     wineWow64Packages.base
